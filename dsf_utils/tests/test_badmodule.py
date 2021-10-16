76c71928a3996aeaabdfd6dc415771533f70b38c
ltsaprounis <leonidas.tsap@gmail.com>
2021-10-16 18:14:54 +0100

    more clean-up

--- dsf_utils/tests/test_badmodule.py
+++
@@ -1,5 +0,0 @@
-from examplepackage.badmodule import bad_function
-
-
-def test_bad_function():
-    assert bad_function(1) == 1
--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ __author__ = ["author name"]
 from setuptools import setup, find_packages
 
 setup(
-    name="examplepackage",
-    version="x.x",
+    name="dsf_utils",
+    version="0.0.0",
     packages=find_packages(),
     description="package description",
     author="author name",
     install_requires=[
-        "numpy>=1.0",
-        "pandas==1.0",
+        "sktime",
     ],
 )
